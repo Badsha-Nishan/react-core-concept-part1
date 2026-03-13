419518170df0769c0577fbc7a59ec286e40da365
Badsha-Nishan <badshanisan14@gmail.com>
2026-03-13 15:28:46 +0600

    added six, four score

--- react-core-concept-part1/src/Batstman.jsx
+++ react-core-concept-part1/src/Batstman.jsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 export default function Batsman() {
   const [run, setRun] = useState(0);
   const [six, setSix] = useState(0);
+  const [four, setFour] = useState(0);
 
   const singleRun = () => {
     const newRun = run + 1;
@@ -10,6 +11,8 @@ export default function Batsman() {
   };
   const fourRun = () => {
     const newRun = run + 4;
+    const fourCount = four + 1;
+    setFour(fourCount);
     setRun(newRun);
   };
   const sixRun = () => {
@@ -22,8 +25,11 @@ export default function Batsman() {
   return (
     <div>
       {run > 50 && <p>Your Score: 50+</p>}
+      {run > 100 && <p>Congrats!! 1st Century</p>}
+      {run > 200 && <p>Congrats!! 2nd Century</p>}
       <h2>Score: {run}</h2>
       <h3>Six: {six}</h3>
+      <h3>Four: {four}</h3>
       <button onClick={singleRun}>Single Run</button>
       <button onClick={fourRun}>Four Run</button>
       <button onClick={sixRun}>Six Run</button>
